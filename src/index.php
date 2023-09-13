ce4d7c03c8a1a446b7a58442e9329112557b3a24
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-09-12 21:16:10 -0500

    Creando el workspace

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Blog videojuegos</title>
+  <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+
+<body>
+  <h1>Esta funcionando!</h1>
+  <div class="alerta">Hola</div>
+</body>
+
+</html>
